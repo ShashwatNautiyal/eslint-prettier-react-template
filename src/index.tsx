a229c7e99f3963307473c9f0d4383cd3ae51e2a7
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2022-10-09 15:59:38 +0530

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
